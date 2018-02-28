4650ce8f9f80342276a581b6f7e699fa40a60072
Andrew Berezovskyi <andriib@kth.se>
2018-02-28 04:57:18 +0100

    Various refactoring
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ src/main/java/org/eclipse/lyo/trs/consumer/handlers/ChangeEventHandler.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2016-2017   KTH Royal Institute of Technology.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ *
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *
+ * Omar Kacimi         -  Initial implementation
+ * Andrew Berezovskyi  -  Lyo contribution updates
+ */
+package org.eclipse.lyo.trs.consumer.handlers;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.jena.rdf.model.Model;
+import org.eclipse.lyo.core.trs.ChangeEvent;
+import org.eclipse.lyo.core.trs.Deletion;
+import org.eclipse.lyo.trs.consumer.util.TrsBasicAuthOslcClient;
+import org.eclipse.lyo.trs.consumer.util.SparqlUtil;
+
+import net.oauth.OAuthException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class for handling the creation of the sparql update for processing a change
+ * event
+ *
+ * @author Omar
+ *
+ */
+public class ChangeEventHandler extends TRSTaskHandler {
+
+    final static Logger logger = LoggerFactory.getLogger(ChangeEventHandler.class);
+    /**
+     * The change event to be processed
+     */
+    ChangeEvent handledChangeEvent;
+    /**
+     * A list of updates passed as an argument to which the sparql update for
+     * processing the change event will be added
+     */
+    List<String> queries;
+    /**
+     * the size of the rdf representation of the rdf member
+     */
+    AtomicLong modelSize;
+
+    public ChangeEventHandler(TrsBasicAuthOslcClient oslcClient, String sparqlQueryService, String sparqlUpdateService,
+            String baseAuth_userName, String baseAuth_pwd, ChangeEvent handledChangeEvent, List<String> queries,
+            AtomicLong modelSize) {
+        super(oslcClient, sparqlQueryService, sparqlUpdateService, baseAuth_userName, baseAuth_pwd);
+        this.handledChangeEvent = handledChangeEvent;
+        threadName = "Change Event for resource: " + handledChangeEvent.getChanged() + " handler thread";
+        this.queries = queries;
+        this.modelSize = modelSize;
+    }
+
+    private void processChangeEvent() throws IOException, OAuthException, URISyntaxException {
+        URI changed = handledChangeEvent.getChanged();
+        logger.debug("creating query for resource " + changed.toString() + " change event ");
+        String query = "";
+        if (handledChangeEvent instanceof Deletion) {
+            query = SparqlUtil.getChangeEventQuery(handledChangeEvent, null);
+            queries.add(query.toString());
+        } else {
+
+            Model updatedResRepresentation = (Model) fetchTRSRemoteResource(changed.toString(), Model.class);
+            if (updatedResRepresentation != null) {
+                modelSize.set(modelSize.get() + updatedResRepresentation.size());
+                query = SparqlUtil.getChangeEventQuery(handledChangeEvent, updatedResRepresentation);
+                queries.add(query.toString());
+            } else {
+                logger.error("could not retrieve representation of member resource with uri: " + changed.toString());
+            }
+        }
+
+        logger.info("finished creating query for resource " + changed.toString() + " change event ");
+    }
+
+    @Override
+    protected void processTRSTask() {
+        try {
+            super.processTRSTask();
+            processChangeEvent();
+        } catch (IOException | OAuthException | URISyntaxException e) {
+            logger.error("Error processing TRS task", e);
+        }
+    }
+
+}
---
+++ src/main/java/org/eclipse/lyo/trs/consumer/mqtt/MqttTrsEventListener.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2017-2018 Xufei Ning and others.
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 which
+ * accompanies this distribution.
+ *
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html and the
+ * Eclipse Distribution License is available at http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *
+ * Xufei Ning          -  Initial implementation
+ * Andrew Berezovskyi  -  Lyo contribution updates
+ */
+
+package org.eclipse.lyo.trs.consumer.mqtt;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.eclipse.lyo.core.trs.ChangeEvent;
+import org.eclipse.lyo.core.trs.Creation;
+import org.eclipse.lyo.core.trs.Deletion;
+import org.eclipse.lyo.core.trs.Modification;
+import org.eclipse.lyo.trs.consumer.handlers.TrsProviderHandler;
+import org.eclipse.lyo.trs.consumer.config.TrsProviderConfiguration;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MqttTrsEventListener implements MqttCallback {
+
+    private final static Logger log = LoggerFactory.getLogger(MqttTrsEventListener.class);
+    private final TrsProviderHandler providerHandler;
+    private final TrsProviderConfiguration providerConfiguration;
+    private final ScheduledExecutorService executorService;
+
+    public MqttTrsEventListener(final TrsProviderHandler providerHandler,
+            final TrsProviderConfiguration providerConfiguration, final ScheduledExecutorService executorService) {
+        this.providerHandler = providerHandler;
+        this.providerConfiguration = providerConfiguration;
+        this.executorService = executorService;
+    }
+
+    public void connectionLost(Throwable throwable) {
+        log.error("Connection with '{}' broker lost", providerConfiguration.getMqttBroker(), throwable);
+    }
+
+    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+        String payload = new String(mqttMessage.getPayload());
+        log.info("Message received: " + payload);
+        // receive "NEW"
+        if (payload.equalsIgnoreCase("NEW")) {
+            log.info("Plain 'NEW' ping message received");
+            executorService.schedule(providerHandler, 0, TimeUnit.MILLISECONDS);
+        }
+        // receive ChangeEvent
+        else {
+            log.info("Full ChangeEvent received");
+            // read data from message
+            String message = payload.substring(payload.indexOf("{") + 1, payload.indexOf("}"));
+            String[] mapper = message.split(" ");
+            int index_order = Arrays.asList(mapper).indexOf("@trs:order");
+            int index_changed = Arrays.asList(mapper).indexOf("@trs:changed");
+            int index_type = Arrays.asList(mapper).indexOf("@rdf:type");
+            String about = mapper[0];
+            String[] changed = mapper[index_changed + 1].split(";"); // changed[0]
+            String[] order = mapper[index_order + 1].split("\""); // order[1]
+            String type = mapper[index_type + 1];
+            // create change event
+            ChangeEvent changeEvent = null;
+            final URI trackedResourceURI = URI.create(changed[0]);
+            final int eventOrder = Integer.parseInt(order[1]);
+            final URI eventURI = URI.create(about);
+            if (type.matches("(.*)Creation(.*)")) {
+                changeEvent = new Creation(eventURI, trackedResourceURI, eventOrder);
+            }
+            if (type.matches("(.*)Modification(.*)")) {
+                changeEvent = new Modification(eventURI, trackedResourceURI, eventOrder);
+            }
+            if (type.matches("(.*)Deletion(.*)")) {
+                changeEvent = new Deletion(eventURI, trackedResourceURI, eventOrder);
+            }
+            log.info(String.format("New event: URI=%s; order=%d", trackedResourceURI, eventOrder));
+            // update change log
+            providerHandler.processChangeEvent(changeEvent);
+        }
+    }
+
+    public void deliveryComplete(IMqttDeliveryToken token) {
+    }
+
+}
